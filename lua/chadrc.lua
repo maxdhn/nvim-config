54e89e76ea9fbdfc365848988dd661c9551ca82c
maxdhn <max.duchan@proton.me>
2025-10-20 15:02:12 +0200

    fix lsp config

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -5,13 +5,21 @@
 ---@type ChadrcConfig
 local M = {}
 
-M.base46 = {
-	theme = "dark_horizon",
+M.ui = {
+  theme = "aquarium",
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+  nvdash = {
+    load_on_startup = true,
+    buttons = {
+      { "  Find Session", "Spc f S", "Telescope persisted" },
+      { "  Find File", "Spc f f", "Telescope find_files" },
+      { "󰈚  Recent Files", "Spc f o", "Telescope oldfiles" },
+      { "󰈭  Find Word", "Spc f w", "Telescope live_grep" },
+      { "  Bookmarks", "Spc m a", "Telescope marks" },
+      { "  Themes", "Spc t h", "Telescope themes" },
+      { "  Mappings", "Spc c h", "NvCheatsheet" },
+    },
+  },
 }
 
 return M
--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,9 +1,6 @@
 -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
-local lspconfig = require "lspconfig"
-
--- EXAMPLE
 local servers = {
   "ansiblels",
   "arduino_language_server",
@@ -28,20 +25,4 @@ local servers = {
   "ts_ls",
   "vimls",
 }
-local nvlsp = require "nvchad.configs.lspconfig"
-
--- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-  }
-end
-
--- configuring single server, example: typescript
--- lspconfig.ts_ls.setup {
---   on_attach = nvlsp.on_attach,
---   on_init = nvlsp.on_init,
---   capabilities = nvlsp.capabilities,
--- }
+vim.lsp.enable(servers)
